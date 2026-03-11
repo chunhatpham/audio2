a57b1dd85a32d0478e02b6b0140b82a15f7b160c
chunhatpham <jickjame84@gmail.com>
2026-03-12 02:37:32 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-        // Đăng ký Service Worker
-        if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('./sw.js')
-            .then(() => console.log('SW Registered'))
-            .catch(err => console.log('SW Registration Failed', err));
-        }
